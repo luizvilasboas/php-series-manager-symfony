f558ff1c798026bde0fe3a349610b988454d572f
Luiz Felipe de Castro Vilas Boas <luizfelipecastrovb@gmail.com>
2023-12-29 11:51:42 -0300

    Create series insertion to the database

---
+++ src/Controller/SeriesController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Series;
+use App\Repository\SeriesRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SeriesController extends AbstractController
+{
+    private SeriesRepository $seriesRepository;
+
+    public function __construct(SeriesRepository $seriesRepository)
+    {
+        $this->seriesRepository = $seriesRepository;
+    }
+
+    #[Route('/series', name: 'app_series', methods: ['GET'])]
+    public function index(): Response
+    {
+        $seriesList = $this->seriesRepository->findAll();
+
+        return $this->render('series/index.html.twig', [
+            'series' => $seriesList,
+        ]);
+    }
+
+    #[Route('/series/create', name: 'app_series_form', methods: ['GET'])]
+    public function addSeriesForm(): Response
+    {
+        return $this->render('series/create.html.twig');
+    }
+
+
+    #[Route('/series/create', name: 'app_series_create', methods: ['POST'])]
+    public function addSeries(Request $request): Response
+    {
+        $seriesName = $request->request->get("name");
+
+        $series = new Series($seriesName);
+
+        $this->seriesRepository->add($series, true);
+
+        return new RedirectResponse('/series');
+    }
+}
