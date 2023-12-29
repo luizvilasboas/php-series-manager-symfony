8b24aa0d877a3f833ccbc1dd4a23fedb058722c0
Luiz Felipe de Castro Vilas Boas <luizfelipecastrovb@gmail.com>
2023-12-29 15:00:15 -0300

    Create edit series feature

--- src/Controller/SeriesController.php
+++ src/Controller/SeriesController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Series;
 use App\Repository\SeriesRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,26 +14,32 @@ use Symfony\Component\Routing\Annotation\Route;
 class SeriesController extends AbstractController
 {
     private SeriesRepository $seriesRepository;
+    private EntityManagerInterface $entityManager;
 
-    public function __construct(SeriesRepository $seriesRepository)
+    public function __construct(SeriesRepository $seriesRepository, EntityManagerInterface $entityManager)
     {
         $this->seriesRepository = $seriesRepository;
+        $this->entityManager = $entityManager;
     }
 
     #[Route('/series', name: 'app_series', methods: ['GET'])]
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $seriesList = $this->seriesRepository->findAll();
-
+        $session = $request->getSession();
+        $successMessage = $session->get('success');
+        $session->remove('success');
+    
         return $this->render('series/index.html.twig', [
             'series' => $seriesList,
+            'successMessage' => $successMessage
         ]);
     }
 
     #[Route('/series/create', name: 'app_series_form', methods: ['GET'])]
     public function addSeriesForm(): Response
     {
-        return $this->render('series/create.html.twig');
+        return $this->render('series/form.html.twig');
     }
 
 
@@ -44,14 +51,38 @@ class SeriesController extends AbstractController
         $series = new Series($seriesName);
 
         $this->seriesRepository->add($series, true);
+        $session = $request->getSession();
+        $session->set('success', 'Série criada com sucesso');
 
         return new RedirectResponse('/series');
     }
 
     #[Route('/series/delete/{id}', name: 'app_series_delete', methods: ['POST'])]
-    public function deleteSeries(int $id): Response
+    public function deleteSeries(int $id, Request $request): Response
     {
         $this->seriesRepository->removeById($id);
+    
+        $session = $request->getSession();
+        $session->set('success', 'Série removida com sucesso');
+
+        return new RedirectResponse('/series');
+    }
+
+    #[Route('/series/edit/{series}', name: 'app_series_edit_form', methods: ['GET'])]
+    public function editSeriesForm(Series $series): Response
+    {
+        return $this->render('series/form.html.twig', compact('series'));
+    }
+    
+    #[Route('/series/edit/{series}', name: 'app_series_edit', methods: ['POST'])]
+    public function editSeries(Series $series, Request $request): Response
+    {
+        $session = $request->getSession();
+        
+        $series->setName($request->request->get('name'));
+        $this->entityManager->flush();
+        
+        $session->set('success', 'Série editada com sucesso');
 
         return new RedirectResponse('/series');
     }
