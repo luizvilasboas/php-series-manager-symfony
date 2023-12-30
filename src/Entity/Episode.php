cc45dc6465d272e38a6e12839843a40b8a475bc3
Luiz Felipe de Castro Vilas Boas <luizfelipecastrovb@gmail.com>
2023-12-30 18:48:06 -0300

    Add check if episode was watched or not

--- src/Entity/Episode.php
+++ src/Entity/Episode.php
@@ -21,6 +21,9 @@ class Episode
     #[ORM\JoinColumn(nullable: false)]
     private Season $season;
 
+    #[ORM\Column]
+    private ?bool $watched = null;
+
     public function __construct(int $number)
     {
         $this->number = $number;
@@ -51,6 +54,18 @@ class Episode
     public function setSeason(?Season $season): static
     {
         $this->season = $season;
+
+        return $this;
+    }
+
+    public function isWatched(): ?bool
+    {
+        return $this->watched;
+    }
+
+    public function setWatched(bool $watched): static
+    {
+        $this->watched = $watched;
 
         return $this;
     }
